ea643afe52a12b495689db1d76f3b4db3e1ba56e
Diana <diana.cracea@stud.ubbcluj.ro>
2024-04-09 12:08:14 +0300

    update

--- scripts.js
+++ scripts.js
@@ -48,8 +48,8 @@ document.addEventListener('DOMContentLoaded', function() {
         return inputGroup;
     }
 });
-
-
+// ----------------------------------------------------------------------------------------
+// API call
 const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector('.search-result');
 const container = document.querySelector('.container');
@@ -61,19 +61,17 @@ searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const ingredients = Array.from(document.querySelectorAll('input[name="ingredient"]')).map(input => input.value);
     searchQuery = ingredients.join('+');
-    const cuisineType = document.getElementById('category').value;
-    const mealType = document.getElementById('category').value;
-    const dietType = document.getElementById('category').value;
-    fetchAPI(cuisineType, mealType, dietType);
+    fetchAPI();
 });
 
-async function fetchAPI(cuisineType, mealType, dietType) {
+async function fetchAPI() {
     const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&to=20`;
     const response = await fetch(baseURL);
     const data = await response.json();
     generateHTML(data.hits);
     console.log(data);
 
+// empty search bar after search
     const firstIngredientInput = document.querySelector('input[name="ingredient"]');
     firstIngredientInput.value = '';
 
@@ -81,28 +79,16 @@ async function fetchAPI(cuisineType, mealType, dietType) {
     for (let i = 1; i < ingredientContainers.length; i++) {
         ingredientContainers[i].remove();
     }
-
-    // Get unique cuisine types, meal types, and diet types from the data
-    const cuisineTypes = [...new Set(data.hits.map(hit => hit.recipe.cuisineType))];
-    const mealTypes = [...new Set(data.hits.map(hit => hit.recipe.mealType))];
-    const dietTypes = [...new Set(data.hits.map(hit => hit.recipe.healthLabels))];
-
-    // Populate select elements with the extracted data
-    populateSelect('cuisineType', cuisineTypes);
-    populateSelect('mealType', mealTypes);
-    populateSelect('dietType', dietTypes);
 }
 
-function populateSelect(selectId, options) {
-    const select = document.getElementById(selectId);
-    select.innerHTML = '<option value="">Select Option</option>';
-    options.forEach(option => {
-        select.innerHTML += `<option value="${option}">${option}</option>`;
-    });
-}
+// ----------------------------------------------------------------------------------------
+// generate HTML
 function generateHTML(results){
     let generatedHTML = '';
-    results.map(result => {
+    results.forEach(result => {
+        const filteredHealthLabels = result.recipe.healthLabels.filter(label => {
+            return ['Vegetarian', 'Vegan', 'Gluten-Free', 'Dairy-Free', 'Egg-Free', 'Soy-Free', 'Alcohol-Free'].includes(label);
+        });
         generatedHTML +=
         `
          <div class="item">
@@ -113,67 +99,34 @@ function generateHTML(results){
             </div>
             <p class="item-data">Cuisine type: ${result.recipe.cuisineType}</p>
             <p class="item-data">Meal type: ${result.recipe.mealType}</p>
-            <p class="item-data">Diet type: ${result.recipe.healthLabels.join(', ')}</p>
+            <p class="item-data">Diet type: ${filteredHealthLabels.join(', ')}</p>
         </div>
         `
     })
     searchResultDiv.innerHTML = generatedHTML;
 }
 
-const categorySelect = document.getElementById('category');
-const subcategorySelect = document.getElementById('subcategory');
+// ----------------------------------------------------------------------------------------
 
-categorySelect.addEventListener('change', function() {
-    const selectedCategory = this.value;
-    if (selectedCategory === '') {
-        subcategorySelect.disabled = true;
-        subcategorySelect.innerHTML = '<option value="">Select Subcategory</option>';
-    } else {
-        // Enable the subcategory select
-        subcategorySelect.disabled = false;
-        // Dynamically populate the subcategories based on the selected category
-        if (selectedCategory === 'cuisineType') {
-            subcategorySelect.innerHTML = `
-                <option value="1_1">american</option>
-                <option value="1_2">asian</option>
-                <option value="1_3">british</option>
-                <option value="1_4">caribbean</option>
-                <option value="1_5">central europe</option>
-                <option value="1_6">chinese</option>
-                <option value="1_7">eastern europe</option>
-                <option value="1_8">french</option>
-                <option value="1_9">greek</option>
-                <option value="1_10">indian</option>
-                <option value="1_11">italian</option>
-                <option value="1_12">japanese</option>
-                <option value="1_13">korean</option>
-                <option value="1_14">kosher</option>
-                <option value="1_15">mediterranean</option>
-                <option value="1_16">mexican</option>
-                <option value="1_17">middle eastern</option>
-                <option value="1_18">nordic</option>
-                <option value="1_19">south american</option>
-                <option value="1_20">south east asian</option>
-                <option value="1_21">world</option>
-            `;
-        } else if (selectedCategory === 'mealType') {
-            subcategorySelect.innerHTML = `
-                <option value="2_1">breakfast</option>
-                <option value="2_2">brunch</option>
-                <option value="2_3">lunch/dinner</option>
-                <option value="2_4">snack</option>
-                <option value="2_5">teatime</option>
-            `;
-        } else if (selectedCategory === 'dietType') {
-            subcategorySelect.innerHTML = `
-                <option value="3_1">vegetarian</option>
-                <option value="3_2">vegan</option>
-                <option value="3_3">soy-free</option>
-                <option value="3_4">gluten-free</option>
-                <option value="3_5">dairy-free</option>
-                <option value="3_6">egg-free</option>
-                <option value="3_7">alcohol-free</option>
-            `;
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    // Select all elements with the class 'filter-option'
+    const filterOptions = document.querySelectorAll('.filter-option');
+
+    // Add event listeners to each filter option
+    filterOptions.forEach(option => {
+        option.addEventListener('click', function() {
+            const value = this.textContent;
+            show(value);
+        });
+    });
+
+    // Define the show function
+    function show(value) {
+        document.querySelector('.textBox').value = value;
+    }
+    let dropdown = document.querySelector('.dropdown');
+    dropdown.onclick = function() {
+        dropdown.classList.toggle('active');
     }
 });
+
